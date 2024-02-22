a69bd4f02b130bb67fbbfc0d8b034dacc878fc50
Arin Faraj <arin.abdul99@gmail.com>
2024-02-22 08:29:36 +0300

    enable focus on mouse hover

--- .config/awesome/signals/init.lua
+++ .config/awesome/signals/init.lua
@@ -1,8 +1,12 @@
+-- Enable sloppy focus, so that focus follows mouse.
+client.connect_signal("mouse::enter", function(c)
+	c:emit_signal("request::activate", "mouse_enter", { raise = false })
+end)
 -- Error handling
-require 'signals.error'
+require("signals.error")
 
 -- Corners
-require 'signals.corners'
+require("signals.corners")
 
 -- Brightness
-require 'signals.brightness'
+require("signals.brightness")
